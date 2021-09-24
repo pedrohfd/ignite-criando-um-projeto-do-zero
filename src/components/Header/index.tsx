16ac9a373ee5530de73f5e306aaa14480f8f0e8c
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2021-09-24 19:28:59 -0300

    feat: header and global styles complete

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,14 @@
+import styles from './header.module.scss';
+import Link from 'next/link';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContent}>
+        <Link href="/">
+          <img src="/images/Logo.svg" alt="logo" />
+        </Link>
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { GetStaticProps } from 'next';
-
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +24,20 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return <h1>Home</h1>;
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: [''],
+    }
+  );
 
-//   // TODO
-// };
+  return {
+    props: { posts },
+  };
+};
